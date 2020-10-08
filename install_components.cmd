064d9b6007ba5fcaa867f1b3e8539d6a1aebf18d
Martin Hertz <mvhertz@gmail.com>
2020-10-08 15:15:28 +0200

    install msvc into main folder and uninstall it from there

--- install_components.cmd
+++ install_components.cmd
@@ -15,7 +15,7 @@ move nsprocess\Plugin\nsProcessW.dll nsis\Plugins\x86-unicode\nsProcess.dll
 move nsprocess\Include\nsProcess.nsh nsis\Include
 curl.exe -k https://git.landicorp.com/electron-downloadtool/electron-downloadtool/-/raw/5da62a7d62329bd9afe7a1bfda3f759d6bc04c80/node_modules/electron-builder/templates/nsis/include/StrContains.nsh > nsis\Include\strContains.nsh
 curl.exe -kO https://download.visualstudio.microsoft.com/download/pr/68d6b204-9df0-4fcc-abcc-08ee0eff9cb2/0b833c703ae7532e54db2d1926e2c3d2e29a7c053358f8c635498ab25bb8c590/vs_BuildTools.exe
-vs_buildtools.exe --quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --wait
+vs_buildtools.exe --quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --installPath "%~dp0msvc" --wait
 del msys2-base-x86_64-latest.* NsProcess.zip vs_BuildTools.exe
 rd /s /q nsprocess
 rd /s /q nsprocess 2>nul
--- uninstall_components.cmd
+++ uninstall_components.cmd
@@ -7,6 +7,4 @@ rd /s /q OpenSSL-Win64
 rd /s /q OpenSSL-Win64 2>nul
 rd /s /q nsis
 rd /s /q nsis 2>nul
-"%programfiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" /uninstall -q
-rd /s /q "%programfiles(x86)%\Microsoft Visual Studio"
-rd /s /q "%programfiles(x86)%\Microsoft Visual Studio" 2>nul
+"%programfiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" --installPath "%~dp0msvc" /uninstall -q
