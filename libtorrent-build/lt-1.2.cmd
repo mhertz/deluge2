f6c1added2742e8c7f96bb353cc81e506d6357c6
Martin Hertz <mvhertz@gmail.com>
2020-11-24 14:59:59 +0100

    fix latest libtorrent 1.2.x building and remove couple  reduandant files

--- libtorrent-build/lt-1.2.cmd
+++ libtorrent-build/lt-1.2.cmd
@@ -1,6 +1,8 @@
 cd "%~dp0"
 cd..
 set BOOST_ROOT=%cd%\boost
+set PYTHON_ROOT=%cd%\python
+set PYTHON_ROOT2="%PYTHON_ROOT:\=/%"
 set OPENSSL=%cd%\OpenSSL-Win64
 set BOOST_BUILD_PATH=%BOOST_ROOT%\tools\build
 set PATH=%PATH%;%BOOST_BUILD_PATH%\src\engine\bin.ntx86;%BOOST_ROOT%;%cd%\python;%cd%\msys64\usr\bin
@@ -8,6 +10,7 @@ mkdir boost & curl -L https://dl.bintray.com/boostorg/release/1.74.0/source/boos
 git clone https://github.com/arvidn/libtorrent -b RC_1_2 lt
 for /f %%i in ('curl -s https://www.python.org/ ^| grep "Latest: " ^| cut -d/ -f5 ^| cut -d" " -f2 ^| tr -d "<"') do set var2=%%i
 for /f %%i in ('echo %var2% ^| cut -d. -f1-2 ^| tr -d .') do set PYTHONVER=%%i
+for /f %%i in ('echo %var2% ^| cut -d. -f1-2') do set PYTHONVER2=%%i
 mkdir python & curl -L https://www.nuget.org/api/v2/package/python/%var2% | bsdtar xf - -C python --include tools --strip-components 1
 msys64\usr\bin\echo -e "Lib\nDLLs\nimport site" >> python\python%PYTHONVER%._pth
 call msvc\VC\Auxiliary\Build\vcvars64.bat
@@ -15,6 +18,7 @@ pushd boost
 call bootstrap.bat
 popd
 pushd lt\bindings\python
+echo using python : %PYTHONVER2% : %PYTHON_ROOT2% : %PYTHON_ROOT2%/include : %PYTHON_ROOT2%/libs ; > %BOOST_BUILD_PATH%\user-config.jam
 b2 crypto=openssl libtorrent-link=static boost-link=static release optimization=space stage_module --abbreviate-paths -j4 address-model=64 openssl-include=%OPENSSL%\include openssl-lib=%OPENSSL%\lib
 popd
 copy /y lt\bindings\python\libtorrent.pyd overlay\Lib\site-packages
