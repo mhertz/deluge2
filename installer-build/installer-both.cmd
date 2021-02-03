36b971dc4030c5d43c40b0579752a028762066db
Martin Hertz <mvhertz@gmail.com>
2021-02-03 13:54:14 +0100

    cleanup

--- installer-build/installer-both.cmd
+++ installer-build/installer-both.cmd
@@ -6,7 +6,6 @@ for /f %%i in ('dir /b /a:d ..\deluge-2* ^| findstr /v dev') do set var1=%%i
 for /f %%i in ('dir /b /a:d ..\deluge-2* ^| findstr dev') do set var2=%%i
 curl https://mailfud.org/geoip-legacy/GeoIP.dat.gz | gzip -d -c > ..\%var1%\GeoIP.dat
 curl https://mailfud.org/geoip-legacy/GeoIP.dat.gz | gzip -d -c > ..\%var2%\GeoIP.dat
-curl https://mailfud.org/geoip-legacy/GeoIPv6.dat.gz | gzip -d -c > ..\%var1%\GeoIPv6.dat
 curl https://mailfud.org/geoip-legacy/GeoIPv6.dat.gz | gzip -d -c > ..\%var2%\GeoIPv6.dat
 del ..\deluge-2*.exe 2>nul
 ..\nsis\makensis /DPROGRAM_VERSION=%var1:~7% /Dsrcdir=..\%var1% deluge-installer.nsi
