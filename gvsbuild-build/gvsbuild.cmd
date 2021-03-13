99cf5f15d44b54e6b74596cb36cfe913a54bff4c
Martin Hertz <mvhertz@gmail.com>
2021-03-13 20:00:42 +0100

    update

--- gvsbuild-build/gvsbuild.cmd
+++ gvsbuild-build/gvsbuild.cmd
@@ -9,7 +9,7 @@ set platform=x64
 set VS_VER=16
 set arch=amd64
 set VSCMD_DEBUG=1
-for /f "delims=#" %%i in ('"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version [16.0^,17.0^) -prerelease -products * -property installationPath') do set MSVC_DIR="%%i"
+for /f "delims=" %%i in ('"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version [16.0^,17.0^) -prerelease -products * -requires Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath') do set MSVC_DIR="%%i"
 for /f %%i in ('curl -s https://www.python.org/ ^| grep "Latest: " ^| cut -d/ -f5 ^| cut -d" " -f2 ^| tr -d "<"') do set var2=%%i
 for /f %%i in ('echo %var2% ^| cut -d. -f1-2 ^| tr -d .') do set PYTHONVER=%%i
 mkdir python & curl -L https://www.nuget.org/api/v2/package/python/%var2% | bsdtar xf - -C python --include tools --strip-components 1
